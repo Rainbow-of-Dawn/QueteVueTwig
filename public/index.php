7d47514476c3077abf9ebd00c4538ff9bc972907
Rainbow-of-Dawn <as.linkpro@gmail.com>
2020-10-26 14:31:07 +0100

    completed

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$products = ['product1','product2', 'product3', 'product4', 'product5'];
+
+$loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
+$twig = new Twig\Environment($loader, [
+    'cache' => false,
+    'debug' => true,
+]);
+
+echo $twig->render("index.html.twig", [
+    'products'=>$products,
+]);
